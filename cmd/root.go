ad8bae839d630415e953838b63004b7a77b7e6d3
Joey Espinosa <jlouis.espinosa@gmail.com>
2019-03-02 01:53:58 -0500

    fix: ensure GOPATH is set to a sane default

--- cmd/root.go
+++ cmd/root.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"errors"
 	"os"
+	"path"
 	"path/filepath"
 
 	db "github.com/ParticleDecay/gowork/pkg/database"
@@ -20,7 +21,11 @@ var (
 
 func init() {
 	oldGoroot := os.Getenv("GOROOT")
-	oldGopath := os.Getenv("GOPATH")
+	oldGopath := path.Join(os.Getenv("HOME"), "go")
+	// ensure GOPATH uses variable if exists
+	if value, ok := os.LookupEnv("GOPATH"); ok {
+		oldGopath = value
+	}
 	rootCmd.PersistentFlags().StringVar(&dbFile, "dbfile", "", "database file (default \"$HOME/.gowork.db\")")
 	rootCmd.PersistentFlags().StringVar(&goroot, "goroot", oldGoroot, "a custom GOROOT")
 	rootCmd.PersistentFlags().StringVar(&gopath, "gopath", oldGopath, "a custom GOPATH")
